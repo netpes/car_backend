2143b655f1c4e724544096a0261788305d1f5854
netpes <arbivnetanel@gmail.com>
2023-05-01 22:32:30 +0300

    cleanup

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const app = express();
 const cors = require("cors");
 const http = require("http").Server(app);
 const port = process.env.PORT || 2000;
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const user = require("./views/user")
 
 
 
-app.set("views", __dirname + "/views");
-app.engine("html", require("ejs").renderFile);
+
 // Put these statements before you define any routes.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
