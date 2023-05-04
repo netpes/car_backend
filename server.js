716d103631afa8d6beb5afdc5dd769d14d61801f
netpes <arbivnetanel@gmail.com>
2023-05-04 21:27:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({origin: "https://carproject.pages.dev/"}));
 app.use("/",user)
-
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 
 
